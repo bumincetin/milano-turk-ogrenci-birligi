b88bf56516a82cdfb2c1a4ab76b115fc218e0bd2
umutarsln <umut@Umut-MacBook-Air.local>
2024-11-07 16:40:18 +0300

    header ve footer her sayfaya eklendi (giriş,kayıt,şifre unttum hariç)

--- src/app/etkinlikler/page.tsx
+++ src/app/etkinlikler/page.tsx
@@ -1,13 +1,15 @@
 import ActivitiesGrid from '@/components/common/activities-section/ActivitiesGrid';
 import ContactSectionCtaWhitePattern5 from '@/components/common/cta-white-pattern/ContactSectionCtaWhitePattern5';
 import IndexSectionHeadersWhitePattern2 from '@/components/common/headers-white-pattern/IndexSectionHeadersWhitePattern2';
+import IndexSectionFootersWhitePattern14 from '@/components/common/footers-white-pattern/IndexSectionFootersWhitePattern14';
 
 export default function Etkinlikler() {
   return (
-    <>
-      <IndexSectionHeadersWhitePattern2 />
-      <ActivitiesGrid />
-      <ContactSectionCtaWhitePattern5 />
-    </>
-  );
+        <>
+            <IndexSectionHeadersWhitePattern2 />
+            <ActivitiesGrid />
+            <ContactSectionCtaWhitePattern5 />
+            <IndexSectionFootersWhitePattern14 />
+        </>
+    );
 }
