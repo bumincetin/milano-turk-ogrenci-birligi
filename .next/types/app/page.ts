e35811f5a295752b8868ad29f1724df134ebc957
umutarsln <umut@Umut-MacBook-Air.local>
2024-11-07 11:26:11 +0300

    gitignore

--- .next/types/app/page.ts
+++ .next/types/app/page.ts
@@ -1,4 +1,4 @@
-// File: /Users/eminkartci/Desktop/codenv/milano-turk-ogrenci-toplulugu/src/app/page.tsx
+// File: /Users/umut/Desktop/repos/milano-turk-ogrenci-toplulugu/src/app/page.tsx
 import * as entry from '../../../src/app/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
